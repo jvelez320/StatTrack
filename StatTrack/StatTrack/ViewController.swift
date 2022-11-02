1e12fee9f9c4f414518b04b76ddf509adc6c3aea
RyanJHamby <68158512+RyanJHamby@users.noreply.github.com>
2022-11-01 20:46:58 -0400

    created Xcode project

---
+++ StatTrack/StatTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StatTrack
+//
+//  Created by Ryan Hamby on 11/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
