8eb0e65835bfe92a7f6ce29e381978f453561fda
RyanJHamby <68158512+RyanJHamby@users.noreply.github.com>
2022-11-05 17:11:25 -0400

    test comment for xcuserdata

--- StatTrack/StatTrack/GameState.swift
+++ StatTrack/StatTrack/GameState.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SQLite
+// comment 2
 // Note: this is not Apple's SQLite3. It is a layer over this SQLite3 for Swift syntax.
 // Reference from: https://github.com/stephencelis/SQLite.swift
 
