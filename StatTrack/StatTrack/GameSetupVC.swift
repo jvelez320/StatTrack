cb54042e441d440b3a62873eca06e5eb721a8035
Kenny Ngo <kngo@umich.edu>
2022-11-13 02:57:28 -0500

    user must fill team name and color to continue with game setup

--- StatTrack/StatTrack/GameSetupVC.swift
+++ StatTrack/StatTrack/GameSetupVC.swift
@@ -13,12 +13,23 @@ final class GameSetupVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var homeTeamName: UITextField!
     @IBOutlet weak var awayTeamName: UITextField!
     @IBOutlet weak var Start: UIButton!
+    @IBOutlet weak var textHelp: UILabel!
+    @IBOutlet weak var homeTeamColor: UIColorWell!
+    @IBOutlet weak var awayTeamColor: UIColorWell!
+    
+    var textFilled: Bool!
+    var colorFilled: Bool!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         overrideUserInterfaceStyle = .light
+        Start.isHidden = true
+        textHelp.isHidden = false
+        textFilled = false
+        colorFilled = false
         gameSetup()
+        setupAddTargetIsNotEmpty()
     }
     
     private func gameSetup() {
@@ -39,13 +50,51 @@ final class GameSetupVC: UIViewController, UITextFieldDelegate {
         return false
     }
     
-    @IBAction func verifyStart(_ sender: Any) {
-        print("I am clicked!")
-        if (homeTeamName.text != "" && awayTeamName.text != "") {
-            print("Yuh")
-            performSegue(withIdentifier: "CameraSetup", sender: nil)
-        } else {
-            print("No can do")
+    func setupAddTargetIsNotEmpty() {
+        Start.isHidden = true
+        textHelp.isHidden = false
+        homeTeamName.addTarget(self, action: #selector(textFieldsNotEmpty), for: .editingChanged)
+        awayTeamName.addTarget(self, action: #selector(textFieldsNotEmpty), for: .editingChanged)
+        homeTeamColor.addTarget(self, action: #selector(colorFieldsNotEmpty), for: .valueChanged)
+        awayTeamColor.addTarget(self, action: #selector(colorFieldsNotEmpty), for: .valueChanged)
+    }
+    
+    @objc func textFieldsNotEmpty(sender: UITextField) {
+        sender.text = sender.text?.trimmingCharacters(in: .whitespaces)
+        
+        guard
+            let homeTeamText = homeTeamName.text, !homeTeamText.isEmpty,
+            let awayTeamText = awayTeamName.text, !awayTeamText.isEmpty
+        else {
+            self.Start.isHidden = true
+            self.textHelp.isHidden = false
+            self.textFilled = false
+            return
+        }
+        textFilled = true
+        toggleStart()
+    }
+    
+    @objc func colorFieldsNotEmpty(sender: UIColorWell) {
+        sender.title = sender.title?.trimmingCharacters(in: .whitespaces)
+        
+        guard
+            let _ = homeTeamColor.selectedColor,
+            let _ = awayTeamColor.selectedColor
+        else {
+            self.Start.isHidden = true
+            self.textHelp.isHidden = false
+            self.colorFilled = false
+            return
+        }
+        colorFilled = true
+        toggleStart()
+    }
+    
+    private func toggleStart() {
+        if textFilled && colorFilled {
+            Start.isHidden = false
+            textHelp.isHidden = true
         }
     }
 }
